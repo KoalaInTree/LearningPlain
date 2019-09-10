1924d1045d8bd43831e468dae19fb1168471db0f
BG389966 <djcao@best-inc.com>
2019-09-10 16:57:54 +0800

    reactor

---
+++ learn/src/main/java/com/djcao/BasicModel.java
@@ -0,0 +1,57 @@
+package com.djcao;
+
+import static com.djcao.SystemConfig.PORT;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+
+/**
+ * @author djcao
+ * @workcode BG389966
+ * @date 2019/9/10
+ */
+public class BasicModel implements Runnable {
+    @Override
+    public void run() {
+        try {
+            ServerSocket ss = new ServerSocket(PORT);
+            while (!Thread.interrupted()){
+                new Thread().start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    static class Handler implements Runnable{
+        final Socket socket;
+        Handler(Socket socket){
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                byte[] input = new byte[SystemConfig.INPUT_SIZE];
+                socket.getInputStream().read(input);
+                socket.getOutputStream().write(process(input));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private byte[] process(byte[] input){
+            byte[] output = "下面是你的输入信息:".getBytes();
+            byte[] result = new byte[input.length+output.length];
+            System.arraycopy(output,0,result,0,output.length);
+            System.arraycopy(input,0,result,output.length,input.length);
+            return result;
+        }
+    }
+
+    public static void main(String[] args) {
+        BasicModel basicModel = new BasicModel();
+        basicModel.run();
+    }
+}
---
+++ learn/src/main/java/com/djcao/SystemConfig.java
@@ -0,0 +1,11 @@
+package com.djcao;
+
+/**
+ * @author djcao
+ * @workcode BG389966
+ * @date 2019/9/10
+ */
+public class SystemConfig {
+    public static final int PORT = 9090;
+    public static final int INPUT_SIZE = 1024;
+}

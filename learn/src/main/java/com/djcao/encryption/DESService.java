6ac8323dd3b9d937cd429343badb3e41e94e96dd
changfaaipiaorou <974441301@qq.com>
2019-09-11 23:28:29 +0800

    des

---
+++ learn/src/main/java/com/djcao/encryption/DESService.java
@@ -0,0 +1,25 @@
+package com.djcao.encryption;
+
+/**
+ * @author djcao
+ * @workcode wb-cdj390654
+ * @date 2019-09-11
+ */
+public class DESService {
+
+    private String secretKey;
+
+    public DESService(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public String encode(String raw){
+
+        return "";
+    }
+
+    public String decode(String encryption){
+
+        return "";
+    }
+}
